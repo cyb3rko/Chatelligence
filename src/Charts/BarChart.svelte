0f642fddccbfd3d659ea491f212b7759bf02a28c
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-06-14 21:43:56 +0200

    feat: BarChart

---
+++ src/Charts/BarChart.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { Chart, ChartData, registerables } from "chart.js";
+  import { onMount } from "svelte";
+  let chartCanvas;
+  let ctx;
+  let chart;
+
+  export let data: ChartData<
+    "bar",
+    {
+      x: number;
+      y: number;
+    }[]
+  > = {
+    datasets: [
+      {
+        label: "Bar Dataset",
+        data: [],
+        backgroundColor: "rgb(255, 99, 132)",
+      },
+    ],
+  };
+
+  onMount(() => {
+    Chart.register(...registerables);
+
+    ctx = chartCanvas.getContext("2d");
+    chart = new Chart(ctx, {
+      type: "bar",
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  });
+
+  $: {
+    if (chart && data.datasets.length > 0) {
+      chart.data = data;
+      chart.update();
+      console.log("Updated chart", data);
+    }
+  }
+</script>
+
+<barChart>
+  <canvas bind:this={chartCanvas} />
+</barChart>
+
+<style>
+  main {
+  }
+</style>
