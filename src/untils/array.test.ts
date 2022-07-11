fd8e641e578323b8e37ab5d3c7ffdd69c5e2426f
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-07-11 12:47:11 +0200

    feat: top function + test

---
+++ src/untils/array.test.ts
@@ -0,0 +1,26 @@
+import { top } from "./array";
+
+const testArray: Array<{ name: string, age: number, credits: number }> = [
+    { name: "Alice", age: 21, credits: 100 },
+    { name: "Bob", age: 22, credits: 200 },
+    { name: "Cebra", age: 30, credits: 300 },
+    { name: "Dario", age: 18, credits: 400 },
+];
+
+const threeOldestSolution = [ testArray[2], testArray[1], testArray[0] ];
+const alphabeticallyOrderedSolution = testArray;
+
+test('top function (order by number)', () => {
+    let threeOldest = top([...testArray], 3, "age");
+    
+    expect(threeOldest.length).toEqual(3);
+    expect(threeOldest).toEqual(threeOldestSolution);
+});
+
+
+test('top function (order by string)', () => {
+    let alphabeticallyOrdered = top([...testArray], 4, "name", false);
+
+    expect(alphabeticallyOrdered.length).toEqual(4);
+    expect(alphabeticallyOrdered).toEqual(alphabeticallyOrderedSolution);
+});
--- src/untils/array.ts
+++ src/untils/array.ts
@@ -1,3 +1,18 @@
 export function emptyArray<T>(length: number, fillValue?: T): T[] {
     return new Array(length).fill(fillValue);
 }
+
+/**
+ * 
+ * @param what What to get the top X of.
+ * @param count The maximum result length. (If the provided array length is smaller it wont be extended)
+ * @param ofWhat The category to sort for.
+ * @param highest Wheather lower or higher values should be on top. True for highest on top.
+ * @returns 
+ */
+export function top<T>(what: Array<T>, count: number, ofWhat: keyof T, highest: boolean = true): T[] {
+    const sorted = what.sort((a, b) => a[ofWhat] < b[ofWhat] ? 1 : -1);
+    if(!highest) sorted.reverse();
+    sorted.length = sorted.length > count ? count : sorted.length;
+    return sorted;
+}
