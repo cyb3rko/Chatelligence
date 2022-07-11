4d86fe910fdd32705048be2f8dd0c3c1dc4ff947
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-07-11 13:31:50 +0200

    fix: top function -> removed sideEffect

--- src/untils/array.ts
+++ src/untils/array.ts
@@ -11,7 +11,7 @@ export function emptyArray<T>(length: number, fillValue?: T): T[] {
  * @returns 
  */
 export function top<T>(what: Array<T>, count: number, ofWhat: keyof T, highest: boolean = true): T[] {
-    const sorted = what.sort((a, b) => a[ofWhat] < b[ofWhat] ? 1 : -1);
+    const sorted = [...what].sort((a, b) => a[ofWhat] < b[ofWhat] ? 1 : -1);
     if(!highest) sorted.reverse();
     sorted.length = sorted.length > count ? count : sorted.length;
     return sorted;
