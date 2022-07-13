70aef7fafb127c80bb5967e322342a8b0f21509b
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-07-13 21:50:29 +0200

    feat: Parser simplified timestap regex

--- src/parser/WhatsAppChatParser.ts
+++ src/parser/WhatsAppChatParser.ts
@@ -9,14 +9,12 @@ export class WhatsAppChatParser extends Parser {
      * regex to match the time stamp
      * a.e. "[31.10.21, 11:58:58]"
      */
-    r_Time =
-        /^(\u200e)*(\[)([0-9]{2}(\.)){2}[0-9]{2}(,)\s{1}[0-2][0-9](:)[0-5][0-9](:)[0-5][0-9](\])/;
+    r_Time = /(\u200e)?(\[\d{2}\.\d{2}\.\d{2}\, \d{2}\:\d{2}\:\d{2}\])/g;
     rg_TimeAndName =
         /(\u200e)*(\[)([0-9]{2}(\.)){2}[0-9]{2}(,)\s{1}[0-2][0-9](:)[0-5][0-9](:)[0-5][0-9](\])(\s){1}[^:]+(: ){1}/g;
 
 
     // TODO: parse special messages beginning with an U+200E
-    // TODO: This is CPU blocking -> use a worker
     async parseRaw(raw: string): Promise<WhatsAppMessage[]> {
         let parsedMessages = [];
         Array.from(raw.matchAll(this.rg_TimeAndName)).forEach((match) => {
