72fb622335319de3c9b6a5808e939a2972642399
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-12-10 20:11:57 +0100

    feat: ExtractionsTable styled

--- src/Charts/ExtractionsTable.svelte
+++ src/Charts/ExtractionsTable.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-    import AscDescSwitch from "../components/AscDescSwitch.svelte";
+    import {
+        Button,
+        Col,
+        Container,
+        FormGroup,
+        Icon,
+        Input,
+        Label,
+        Row,
+        Table,
+    } from "sveltestrap";
     import { top } from "../utils/array";
     import type { Extraction } from "../utils/processor.types";
 
@@ -9,32 +19,69 @@
 
     let ascending = true;
     let page = 1;
+    const pageSize = 10;
+    let totalExtractions = extractions.length;
 
     $: {
-        display = top(extractions, 10, (page - 1) * 10, "mentions", ascending);
+        display = top(
+            extractions,
+            pageSize,
+            (page - 1) * pageSize,
+            "mentions",
+            ascending,
+        );
     }
 </script>
 
-<container>
-    <table>
-        <tr><th>{title}</th><th>Mentions</th><th>See all</th></tr>
-        {#each display as ext}
-            <tr>
-                <td>{ext.extracted}</td>
-                <td>{ext.mentions.length}</td>
-                <td>//TODO: Implement mentions view</td>
-            </tr>
-        {/each}
-    </table>
-    <settings>
-        <AscDescSwitch bind:asc={ascending} />
-        <label for="page">Page</label>
-        <input
-            type="number"
-            name="page"
-            min={1}
-            max={Math.ceil(extractions.length / 10)}
-            bind:value={page}
-        />
-    </settings>
-</container>
+<Container>
+    <Row cols={2}>
+        <FormGroup>
+            <Input type="switch" label="Aufsteigend" bind:checked={ascending} />
+        </FormGroup>
+        <FormGroup>
+            <Row>
+                <Col sm={{ size: 1 }} class="align-middle">
+                    <Label for="page" style="margin-top: 0.25em;">Page:</Label>
+                </Col>
+                <Col>
+                    <Input
+                        type="number"
+                        name="page"
+                        min={1}
+                        max={Math.ceil(extractions.length / 10)}
+                        bind:value={page}
+                    />
+                </Col>
+            </Row>
+        </FormGroup>
+    </Row>
+    <Row>
+        <Table striped size="sm">
+            <thead>
+                <th>#</th>
+                <th>{title}</th>
+                <th>Mentions</th>
+                <th>See all</th>
+            </thead>
+            <tbody>
+                {#each display as ext, i}
+                    <tr>
+                        <th scope="row">
+                            {ascending
+                                ? pageSize * (page - 1) + i + 1
+                                : totalExtractions - i - pageSize * (page - 1)}
+                        </th>
+                        <td>{ext.extracted}</td>
+                        <td>{ext.mentions.length}</td>
+                        <td>
+                            <Button color="info" disabled>
+                                <Icon name="pie-chart-fill" />
+                            </Button>
+                            <Button color="secondary" disabled>Mentions</Button>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </Table>
+    </Row>
+</Container>
