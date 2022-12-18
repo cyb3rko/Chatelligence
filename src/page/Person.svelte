1014b211c9733d55936dac755c4e96f482b3b819
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-12-19 00:56:24 +0100

    feat: Person Page language pi chart

--- src/page/Person.svelte
+++ src/page/Person.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
+    import PiChart from "../Charts/PiChart.svelte";
+    import { generateUiColorFromString } from "../utils/color";
     import type { Analysis } from "../webWorker/processor.worker";
 
     export let id: string;
     export let analysis: Analysis;
+
+    let senderLanguages = analysis?.senderLanguages?.[id]
+        ? Object.entries(analysis.senderLanguages[id])
+        : [];
 </script>
 
 <person>
     <h2>{id}</h2>
 
-    {#each Object.entries(analysis.senderLanguages[id]) as lang}
+    <PiChart
+        data={{
+            labels: senderLanguages.map((l) => l[0]),
+            datasets: [
+                {
+                    data: senderLanguages.map((l) => l[1]),
+                    backgroundColor: senderLanguages.map((l) =>
+                        generateUiColorFromString(l[0]),
+                    ),
+                },
+            ],
+        }}
+    />
+
+    {#each senderLanguages as lang}
         {lang[0]}: {lang[1]}
         <br />
     {/each}
