b510223ffe1278191be1f8c87ea633713c98ce4d
Morten Renner <68235628+Morten-Renner@users.noreply.github.com>
2022-12-21 20:27:06 +0100

    feat: toPercent

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,7 @@
 export function round(num: number, decimals: number) {
-    return Math.round(num / Math.pow(10, decimals)) * Math.pow(10, decimals);
+    return Math.round(num * Math.pow(10, decimals)) / Math.pow(10, decimals);
+}
+
+export function toPercent(num: number, decimals: number) {
+    return round(num * 100, decimals);
 }
